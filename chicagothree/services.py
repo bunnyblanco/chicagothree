6e288d84406bbecd11f781322a47d06347d61a74
Bruce C. Paul <bruce.c.paul@gmail.com>
2015-05-20 22:00:38 -0500

    Have a basic query working.

--- chicagothree/services.py
+++ chicagothree/services.py
@@ -27,17 +27,33 @@ def get_service_dict():
             services[tag[1]] = opt.text
     return services
 
-def get_service_url(tag):
+def get_loc_service_url(tag):
     """
      Get an appropriately formed URL to request the initial form needed
     to submit a service request for the service identified by the tag.
+    Specifically for location based services.
     """
     url = 'https://'
     params = {}
     params['op'] = 'locform'
-    params['invserv'] = tag
+    params['InvSRType'] = tag
 #...
     r = requests.get(base_url, params=params)
     if r.status_code==200:
         url = r.url
     return url
+
+def get_search_url(query):  #This one needs some work
+    """
+    Form a URL to generate a list of potential service types.
+    """
+    url = 'https://'
+    params = {}
+    params['op'] = 'query'
+#    params['InvSRType'] = tag
+#...
+    r = requests.post(base_url, params=params, data=query)
+    if r.status_code==200:
+        url = r.url
+    return url
+
--- sample_request.py
+++ sample_request.py
@@ -5,4 +5,9 @@ if len(tags)>0:
     for k, v in tags.items():
         print k, v
 
+if len(tags)>0:
+    tag = tags.items()[0][0]
+    url = services.get_loc_service_url('PCE')
+    print url
 
+print services.get_search_url('PCE')
